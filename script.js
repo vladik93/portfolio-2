fe36daa4defc26bbb491b6a6ac6571c4e837a639
Vladi <39379713+vladik93@users.noreply.github.com>
2024-02-24 20:57:23 +0200

    worked on menu social links + transition

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const headerMenuBtn = document.getElementById('header-menu-button');
 const headerMenuEl = document.getElementById('header-menu');
 
+const menuNavItemEls = document.querySelectorAll('.menu-nav-item');
+const menuSocialItemEls = document.querySelectorAll('.menu-social-item');
+
+
+
 let isMenuActive = false;
 
 
@@ -11,9 +16,26 @@ headerMenuBtn.addEventListener('click', () => {
   if(isMenuActive) {
     headerMenuEl.classList.add('show');
     headerMenuBtn.classList.add('active');  
+
+    menuNavItemEls.forEach((item) => {
+      item.classList.add('show');
+    })
+
+    menuSocialItemEls.forEach(item => {
+      item.classList.add('show');
+    })
   } else {
     headerMenuEl.classList.remove('show');
     headerMenuBtn.classList.remove('active')
+    
+    menuNavItemEls.forEach((item) => {
+      item.classList.remove('show');
+
+      menuSocialItemEls.forEach(item => {
+        item.classList.remove('show');
+      })
+    })
+
   }
 })
 
